4233a2bde69665332c97551656e3c1473d02283a
Manik Malhotra <malhotramanik@gmail.com>
2023-01-09 06:40:52 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:edit, :new, :create, :update]
+  resources :microposts,          only: [:create, :destroy]
+  get '/microposts', to: 'static_pages#home'
 end
